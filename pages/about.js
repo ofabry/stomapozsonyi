4c2948c1d753dd9847f76ff02bd694cf5c47b562
Ondrej Fabry <ondrej@fabry.email>
2021-05-20 21:07:52 +0200

    Update about.js

--- pages/about.js
+++ pages/about.js
@@ -4,28 +4,22 @@ const About = ({ title, description, ...props }) => {
   return (
     <>
       <Layout pageTitle={`${title} | About`} description={description}>
-        <h1 className="title">Welcome to this demo blog!</h1>
+        <h1 className="title">Stoma Pozsonyi</h1>
 
         <p className="description">
-          This is a simple blog built with Next, easily deployable on{' '}
-          <a href="https://url.netlify.com/r1j6ybSYU">Netlify</a>.
+          tel. č.: 031/7878106
+          email: stomavp@gmail.com
         </p>
 
         <p>
-          You can check out the{' '}
-          <a href="https://github.com/cassidoo/next-netlify-blog-starter">
-            repo here.
-          </a>{' '}
-          If you'd like to build it yourself,{' '}
-          <a href="https://url.netlify.com/ByVW0bCF8">
-            here is a tutorial on how to do so
-          </a>
-          !
+          Adresa:
+          Školská 1565
+          925 22 Veľké Úľany
         </p>
 
         <p>
-          This project includes a basic layout and header, base styles, dynamic
-          routing with getStaticPaths, and posts saved as Markdown.
+          instagram: stomavp
+          facebook: ?
         </p>
       </Layout>
     </>
